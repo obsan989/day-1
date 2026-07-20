b17e80a60a5d267901bbc418b04225c26c7e4b52
obsan <obsanadugna8@gmail.com>
2026-07-20 15:35:42 +0300

    created two file

---
+++ try.py
@@ -0,0 +1,17 @@
+student_name = "Obsan Adugna"
+age = 22 
+balance =1500.50
+is_enrolled = True
+verified = None
+int
+float
+str
+bool
+None
+age_text = input ("22")
+age = int (age_text)
+next_year = age + 1
+balance = 1500
+is_member = True
+balance = 1500
+balance>
